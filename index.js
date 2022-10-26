92bdd3f5488eed6c15eb86b468bbb1bd87ebdaf4
DJTechnoo <askeltechno@hotmail.com>
2022-10-26 12:59:08 +0200

    Changed key.

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ window.addEventListener('DOMContentLoaded', () => {
         },
 
         data: {
-            googleAPIKey: 'AIzaSyCrqWMjYCqixgDg_vQB745FRJ53pMFb57s',
+            googleAPIKey: 'AIzaSyAy4F5nyfepQCRFfr_JqxJBo01NeIR7i5U',
             googleSpreadsheetKey: '1jwQptUEL1o23PZrET81PUIIAIS6H9eSL88rQhuQ2eXs',
             error: console.error,
             parsed: function (columns) {
